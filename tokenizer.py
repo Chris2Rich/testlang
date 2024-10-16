bfe83796cd0f219cb48869b3dc2f1d49df9a17da
Chris Zac <cz07business@gmail.com>
2024-10-16 10:03:52 +0000

    !!!

--- tokenizer.py
+++ tokenizer.py
@@ -1,6 +1,7 @@
 import argparse
 # example syntax
-# (i64) dot_product()
+# (i64) dot_product(i64array a, i64array b) => (reduce<+> *) where (len a == len b)
+# (ui5array) cypher(ui5array s, i5 n) => (- s / 2 n +)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input")
@@ -10,5 +11,14 @@ args = parser.parse_args()
 i_file = open(args.input, "r")
 i_text = i_file.readlines()
 i_file.close()
+
+tokens = {
+    tok_eof : -1,
+    tok_identifier : -2,
+    tok_number : -3,
+}
+
+id_str = str()
+num_val = float()
 
 print(i_text)
