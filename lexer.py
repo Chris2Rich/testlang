9d6347d89abcafde99b5ecb4131fc0c737dbfa83
chri2rich <cz07business@gmail.com>
2024-10-19 10:10:18 +0100

    !

--- lexer.py
+++ lexer.py
@@ -54,85 +54,89 @@ tok_stream = []
 class tokenizer:
   c = 0
   def Next(self):
-    i = ""
-    if self.c >= len(s):
-      yield ("eof", tok["eof"])
-    while s[self.c] == " ":
-      self.c += 1
-    if s[self.c] == "/":
-      #skip self.comments
-      if s[self.c+1] == "/":
-        while s[self.c] != "\n":
-          self.c += 1
-      else:
+    try:
+      i = ""
+      if self.c >= len(s):
+        yield ("eof", tok["eof"])
+      while s[self.c] == " ":
         self.c += 1
-        yield (s[self.c-1], tok["divide"])
-    if s[self.c] == "\n":
-      self.c += 1
-      yield (s[self.c-1], tok["newline"])
-    if s[self.c] == ",":
-      self.c += 1
-      yield (s[self.c-1], tok["delimiter"])
-    if s[self.c] == "+":
-      self.c += 1
-      yield (s[self.c-1], tok["add"])
-    if s[self.c] == "-":
-      self.c += 1
-      yield (s[self.c-1], tok["subtract"])
-    if s[self.c] == "*":
-      self.c += 1
-      yield (s[self.c-1], tok["multiply"])
-    if s[self.c] == ";":
-      self.c += 1
-      yield (s[self.c-1], tok["pop"])
-    if s[self.c] == ":":
-      self.c += 1
-      yield (s[self.c-1], tok["flip"])
-    if s[self.c] == "#":
-      self.c += 1
-      yield (s[self.c-1], tok["dupe"])
-    if s[self.c] == "<":
-      self.c += 1
-      yield (s[self.c-1], tok["left_arr"])
-    if s[self.c] == ">":
-      self.c += 1
-      yield (s[self.c-1], tok ["right_arr"])
-    if s[self.c] == "(":
-      self.c += 1
-      yield (s[self.c-1], tok["left_bra"])
-    if s[self.c] == ")":
-      self.c += 1
-      yield (s[self.c-1], tok["right_bra"])
-    if s[self.c] == "[":
-      self.c += 1
-      yield (s[self.c-1], tok["left_squ"])
-    if s[self.c] == "]":
-      self.c += 1
-      yield (s[self.c-1], tok["right_squ"])
-    if s[self.c] == "{":
-      self.c += 1
-      yield (s[self.c-1], tok["right_cur"])
-    if s[self.c] == "}":
-      self.c += 1
-      yield (s[self.c-1], tok["left_cur"])
-    if s[self.c] == "=":
-      self.c += 1
+      if s[self.c] == "/":
+        #skip self.comments
+        if s[self.c+1] == "/":
+          while s[self.c] != "\n":
+            self.c += 1
+        else:
+          self.c += 1
+          yield (s[self.c-1], tok["divide"])
+      if s[self.c] == "\n":
+        self.c += 1
+        yield (s[self.c-1], tok["newline"])
+      if s[self.c] == ",":
+        self.c += 1
+        yield (s[self.c-1], tok["delimiter"])
+      if s[self.c] == "+":
+        self.c += 1
+        yield (s[self.c-1], tok["add"])
+      if s[self.c] == "-":
+        self.c += 1
+        yield (s[self.c-1], tok["subtract"])
+      if s[self.c] == "*":
+        self.c += 1
+        yield (s[self.c-1], tok["multiply"])
+      if s[self.c] == ";":
+        self.c += 1
+        yield (s[self.c-1], tok["pop"])
+      if s[self.c] == ":":
+        self.c += 1
+        yield (s[self.c-1], tok["flip"])
+      if s[self.c] == "#":
+        self.c += 1
+        yield (s[self.c-1], tok["dupe"])
+      if s[self.c] == "<":
+        self.c += 1
+        yield (s[self.c-1], tok["left_arr"])
       if s[self.c] == ">":
         self.c += 1
-        yield (s[self.c-2] + s[self.c-1], tok["binding"])
-      else:
-        yield (s[self.c-1], tok["equality"])
-    else:
-      while not s[self.c] in " +=*/<>(){}[],;:#" and self.c != len(s):
-        i += s[self.c]
+        yield (s[self.c-1], tok ["right_arr"])
+      if s[self.c] == "(":
         self.c += 1
-      if i.isnumeric() or (i.replace(".", "").isnumeric() and i.count(".") == 1):
-        yield (i, tok["number_literal"])
-      elif (i[0] == "\"" and i[-1] == "\"") or (i[0] == "\'" and i[-1] == "\'"):
-        yield (i, tok["string_literal"])
+        yield (s[self.c-1], tok["left_bra"])
+      if s[self.c] == ")":
+        self.c += 1
+        yield (s[self.c-1], tok["right_bra"])
+      if s[self.c] == "[":
+        self.c += 1
+        yield (s[self.c-1], tok["left_squ"])
+      if s[self.c] == "]":
+        self.c += 1
+        yield (s[self.c-1], tok["right_squ"])
+      if s[self.c] == "{":
+        self.c += 1
+        yield (s[self.c-1], tok["right_cur"])
+      if s[self.c] == "}":
+        self.c += 1
+        yield (s[self.c-1], tok["left_cur"])
+      if s[self.c] == "=":
+        self.c += 1
+        if s[self.c] == ">":
+          self.c += 1
+          yield (s[self.c-2] + s[self.c-1], tok["binding"])
+        else:
+          yield (s[self.c-1], tok["equality"])
       else:
-        yield (i, tok["identifier"])
-      self.c += 1
+        while not s[self.c] in " +=*/<>(){}[],;:#" and self.c != len(s):
+          i += s[self.c]
+          self.c += 1
+        if i.isnumeric() or (i.replace(".", "").isnumeric() and i.count(".") == 1):
+          yield (i, tok["number_literal"])
+        elif (i[0] == "\"" and i[-1] == "\"") or (i[0] == "\'" and i[-1] == "\'"):
+          yield (i, tok["string_literal"])
+        else:
+          yield (i, tok["identifier"])
+        self.c += 1
+    except Exception as err:
+      print(f"Error on line: {self.c}. Traceback => \n {err}")
+      raise
 
 toker = tokenizer()
 t = next(toker.Next(), None)
