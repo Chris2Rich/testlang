ae9b06e3d2335c332860ac4ccd0c28cd3b727743
Chris2rich <azkronthegamer@gmail.com>
2026-02-03 19:46:39 +0000

    !

--- lib/math.cpp
+++
@@ -1,6 +0,0 @@
-inline constexpr double sqrt(double n, double res = 1){
-    for(int i = 0; i < 1000; i++){
-        res = (res / 2.0) + (n / (2.0 * res));
-    }
-    return res;
-}
--- spec/mvp.md
+++
@@ -1,2 +0,0 @@
-no types
-simple scripting language
